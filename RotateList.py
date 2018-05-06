e72fd917ac79ba146bf1eeae7cdba0708b13c8ae
Uriel Ulloa <uulloaadan@middlebury.edu>
2018-05-06 03:48:01 -0700

    Update RotateList.py

--- RotateList.py
+++ RotateList.py
@@ -12,12 +12,6 @@ Input: 0->1->2->NULL, k = 4
 Output: 2->0->1->NULL
 """
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def rotateRight(self, head, k):
         """
@@ -29,32 +23,27 @@ class Solution(object):
         if head is None or k == 0 or head.next is None:
             return head
         
-        current = head
-        length = 1
         
         #Loop to get the length of the linked-list. O(n)
-        #This loop is necessary to compute k%length of list which tells us whihch element is the alst element in our list
+        #This loop is necessary to compute k%length of list which tells us whihch element is the last element in our list
+        current = head
+        length = 1
         while current.next is not None:
             length +=1
             current= current.next
-        
+        end = current
+            
+        #Second loop to find the the cutt-off point of the list. < O(n-k%n)
         count = 1   
         current = head
-        front = head
-         
-        #Second loop through the list to modify list into the solution. O(n)
-        while count <= length:
-            #This will be the last element in our solution
-            if count == length - k%length:  
-                end = current
-            #Cut of the list from the element we marked as the end and place it at the front of the list.
-            if count == length:
-                current.next = front
-                front = end.next
-                end.next = None
-            
+        while count < length - k%length:
             current = current.next
-            count += 1
-
-        return front 
+            count += 1 
+        #Found the cutt-off point
+        #Make the current element the end of the list and place the elements that used to be next at the front of the list
+        end.next = head
+        front = current.next
+        current.next = None
+        
+        return front
         
